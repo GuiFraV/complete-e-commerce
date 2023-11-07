848a34733a85063611861657f14350c02c00f6aa
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-11-07 16:20:39 +0100

    3)Librairie : add material ui /icon

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className="left">
+
+        </div>
+        <div className="center"></div>
+        <div className="right"></div>
+      </div>
+    </div>
   )
 }
 
